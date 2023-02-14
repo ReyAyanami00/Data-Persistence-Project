56c8c0ffcd6229429dce6257dc05dbbe8a846c4d
ReyAyanami00 <patinhodeborrachaxixi@gmail.com>
2023-02-14 14:27:51 +0000

    update

---
+++ Assets/Scripts/MenuUI.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUI : MonoBehaviour
+{
+ 
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void InputName(string s)
+    {
+        GameManager.Instance.userName = s;
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+    Application.Quit();
+#endif
+    }
+}
